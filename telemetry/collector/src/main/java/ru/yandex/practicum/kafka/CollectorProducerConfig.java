1e8ed5793717c03e7d71949b88cace72c2acb228
luteks <sebyackin.daniil@ya.ru>
2025-09-30 12:57:19 +0300

    fix

--- telemetry/collector/src/main/java/ru/yandex/practicum/kafka/CollectorProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/kafka/CollectorProducerConfig.java
@@ -7,12 +7,10 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.Properties;
 
 @Configuration
-@PropertySource("classpath:application.yaml")
 public class CollectorProducerConfig {
 
     @Value("${collector.kafka.producer.properties.bootstrap.servers}")
